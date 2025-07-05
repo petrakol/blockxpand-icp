55905a48a331d16e92cc8fb7ef4b4ea023f7710f
petrakol <78788008+petrakol@users.noreply.github.com>
2025-07-05 23:00:42 +0200

    Add InfinitySwap adapter and mock vault

---
+++ src/mock_infinity_canister/src/lib.rs
@@ -0,0 +1,41 @@
+use candid::{CandidType, Nat, Principal};
+use ic_cdk_macros::query;
+use serde::Deserialize;
+
+#[derive(CandidType, Deserialize, Clone)]
+struct Position {
+    ledger: Principal,
+    subaccount: Vec<u8>,
+}
+
+#[candid::candid_method(query)]
+#[query]
+fn get_user_positions(_p: Principal) -> Vec<Position> {
+    vec![Position {
+        ledger: ic_cdk::api::id(),
+        subaccount: vec![0; 32],
+    }]
+}
+
+#[candid::candid_method(query)]
+#[query]
+fn icrc1_metadata() -> Vec<(String, candid::types::value::IDLValue)> {
+    vec![
+        (
+            "icrc1:symbol".to_string(),
+            candid::types::value::IDLValue::Text("INF".to_string()),
+        ),
+        (
+            "icrc1:decimals".to_string(),
+            candid::types::value::IDLValue::Nat8(8),
+        ),
+    ]
+}
+
+#[candid::candid_method(query)]
+#[query]
+fn icrc1_balance_of(_a: (Principal, Option<Vec<u8>>)) -> Nat {
+    Nat::from(1_000_000_000u64)
+}
+
+ic_cdk::export_candid!();
