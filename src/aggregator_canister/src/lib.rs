80487636d04263d87e04ec88e8ce302ba45c177d
petrakol <78788008+petrakol@users.noreply.github.com>
2025-07-07 00:21:51 +0200

    Fix cfg on canister init

--- src/aggregator_canister/src/lib.rs
+++ src/aggregator_canister/src/lib.rs
@@ -2,7 +2,9 @@ pub use aggregator::*;
 
 #[ic_cdk_macros::init]
 fn init() {
+    #[cfg(not(target_arch = "wasm32"))]
     ic_cdk::spawn(async { aggregator::utils::load_dex_config().await });
+    #[cfg(not(target_arch = "wasm32"))]
     aggregator::utils::watch_dex_config();
     ic_cdk::spawn(async { aggregator::pool_registry::refresh().await });
     aggregator::pool_registry::schedule_refresh();
