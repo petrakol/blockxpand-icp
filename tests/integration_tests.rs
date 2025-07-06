4f4eaccb2042c6e02ed9159a6d4e92f9098e7aac
petrakol <78788008+petrakol@users.noreply.github.com>
2025-07-06 11:49:47 +0200

    Fix conditional import in integration tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,7 +1,9 @@
 #[cfg(test)]
 mod tests {
     use blockxpand_icp::{get_holdings, Holding};
-    use candid::{Decode, Encode, Nat, Principal};
+    use candid::{Decode, Encode, Principal};
+    #[cfg(feature = "claim")]
+    use candid::Nat;
     use ic_agent::{identity::AnonymousIdentity, Agent};
     use std::io::Write;
     use std::path::Path;
